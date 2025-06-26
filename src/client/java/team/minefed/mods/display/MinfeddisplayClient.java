39c77b6cf487b580ec00f1162cb69111cada9e77
Chan Kang <me@kentakang.com>
2025-06-26 15:09:19 +0900

    feat: initial commit

---
+++ src/client/java/team/minefed/mods/display/MinfeddisplayClient.java
@@ -0,0 +1,10 @@
+package team.minefed.mods.display;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinfeddisplayClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/team/minefed/mods/display/MinfeddisplayDataGenerator.java
@@ -0,0 +1,11 @@
+package team.minefed.mods.display;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinfeddisplayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
