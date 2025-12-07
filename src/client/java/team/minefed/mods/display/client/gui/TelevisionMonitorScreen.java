3edcd7647c340616179d6544de7cc9ebb90689d8
Chan Kang <me@kentakang.com>
2025-12-07 15:42:05 +0900

    fix: fix url maxLength

--- src/client/java/team/minefed/mods/display/client/gui/TelevisionMonitorScreen.java
+++ src/client/java/team/minefed/mods/display/client/gui/TelevisionMonitorScreen.java
@@ -25,9 +25,9 @@ public class TelevisionMonitorScreen extends Screen {
     protected void init() {
         super.init();
         this.urlField = new TextFieldWidget(this.textRenderer, this.width / 2 - 100, this.height / 2 - 10, 200, 20, Text.of(""));
+        this.urlField.setMaxLength(256);
         this.urlField.setText(this.currentUrl);
         this.urlField.setPlaceholder(Text.translatable("gui.television_monitor.url"));
-        this.urlField.setMaxLength(256);
         this.addDrawableChild(this.urlField);
 
         this.addDrawableChild(ButtonWidget.builder(Text.of("Save"), button -> {
